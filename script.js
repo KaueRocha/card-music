ddc2a957438bde9d263fd8e199e036058e0e7066
KaueRocha <kauerocha.o@hotmail.com>
2022-05-12 06:46:54 -0300

    função JS do modal

---
+++ script.js
@@ -0,0 +1,11 @@
+function openModal() {
+    const modal = document.querySelector('.modal')
+
+    modal.style.display = "block"
+}
+
+function closedModal() {
+    const modal = document.querySelector('.modal')
+
+    modal.style.display = "none"
+}
